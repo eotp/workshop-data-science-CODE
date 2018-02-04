7e5efed4fde8d2aed659ed7a0325646200cdbda9
eotp <krois@zedat.fu-berlin.de>
2018-02-04 09:00:36 +0100

    scipy nb

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -10,7 +10,7 @@ PATH2DATA = os.path.abspath(os.path.join(os.getcwd(), os.pardir, 'datasets'))
 
 def load_chicken():
     df = pd.read_csv(os.path.join(PATH2DATA, "chicken.csv"))
-    chicken = pd.DataFrame(df.groupby(["Chick", "Diet"])["weight"].mean()).reset_index()
+    chicken = pd.DataFrame(df.groupby(["Chick", "Diet"])["weight"].mean().round().astype(int)).reset_index()
 
     diets = chicken.groupby(["Diet", "Chick"])["weight"].mean().reset_index()
     chick_diet1 = diets.loc[diets["Diet"]==1, "weight"].values
@@ -19,3 +19,20 @@ def load_chicken():
     chick_diet4 = diets.loc[diets["Diet"] == 4, "weight"].values
 
     return chicken, chick_diet1, chick_diet2, chick_diet3, chick_diet4
+
+
+def data_exponential(y0=2.5, m=-4.0, C=2.0, n=25):
+    import numpy as np
+    np.random.seed(1234)
+    # Generate some data based on these
+    x_min, x_max = 0, 1
+    x = np.linspace(x_min, x_max, n)
+
+    def model_func(x, y0, m, C):
+        return y0 * np.exp(m * x) + C
+
+    y = model_func(x, y0, m, C)
+    # Add noise
+    y = y + 0.75 * (np.random.random(n) - 0.5)
+
+    return x, y
