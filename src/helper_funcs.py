ca4eb000f14e97f8e12a93efab348d7e59194a99
eotp <krois@zedat.fu-berlin.de>
2018-02-11 22:46:51 +0100

    eda

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -37,13 +37,14 @@ def data_exponential(y0=2.5, m=-4.0, C=2.0, n=25):
     return x, y
 
 
-def cuteplot(gpd, crs="Mollweide", title=None):
+def cuteplot(gpd_df, crs="Mollweide", title=None, kdp=False, map_extend=(-27, 45, 33, 73.5), bw=0.2):
     '''
     Function to leverage the cartopy library to plot geographical data
     '''
     import matplotlib.pyplot as plt
     import cartopy.crs as ccrs
     import cartopy.feature as cfeature
+    import seaborn as sns
 
     # map projections
     proj = {"PlateCarree": ("+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs",
@@ -58,7 +59,7 @@ def cuteplot(gpd, crs="Mollweide", title=None):
             }
 
     # plot figure
-    plt.figure()
+    fig = plt.figure()
     ax = plt.axes(projection=proj.get(crs)[1])
     # add features
     ax.coastlines(resolution='50m')
@@ -71,12 +72,23 @@ def cuteplot(gpd, crs="Mollweide", title=None):
     ax.add_feature(countries)
 
     # extend
-    ax.set_extent((-27, 45, 33, 73.5))
+    ax.set_extent(map_extend)
     # plot geopandas data frame
     if crs != "PlateCarree":
-        gpd = gpd.to_crs(proj.get(crs)[0])
+        gpd_df = gpd_df.to_crs(proj.get(crs)[0])
 
-    gpd.plot(ax=ax, marker='o', color='red',
+    if kdp:
+        kdp_germany = sns.kdeplot(gpd_df['Target Longitude'],
+                                  gpd_df['Target Latitude'],  # y-axis
+                                  cmap="viridis",
+                                  shade=True,
+                                  shade_lowest=False,
+                                  bw=bw,
+                                  transform = ccrs.PlateCarree()
+                                  )
+        kdp_germany.plot()
+    else:
+        gpd_df.plot(ax=ax, marker='o', color='red',
              markersize=5, alpha=0.01)
     # add gridlines
     ax.gridlines()
