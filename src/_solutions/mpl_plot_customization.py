9fcc8c36259b7db7360f706c3d595502948d252f
eotp <krois@zedat.fu-berlin.de>
2018-03-08 22:43:17 +0100

    working on notebooks

--- src/_solutions/mpl_plot_customization.py
+++ src/_solutions/mpl_plot_customization.py
@@ -2,7 +2,7 @@
 fig, ax = plt.subplots(figsize=(10,6))
 ax.plot(X, C, color="green", label="cosine")
 ax.plot(X, S, linestyle="--", linewidth=3.5, label="sine")
-aax.set_title("My awesome matplotlib figure", size=16)
+ax.set_title("My awesome matplotlib figure", size=16)
 ax.legend(fontsize=12)
 ax.set_xlim([-4,4])
 ax.set_xticks([-np.pi, 0, np.pi])
