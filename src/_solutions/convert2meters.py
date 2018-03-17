bd134ae07e26aa40fa099d9a3a5b1fa7b235093d
eotp <krois@zedat.fu-berlin.de>
2018-03-17 19:17:23 +0100

    update code base

--- src/_solutions/convert2meters.py
+++ src/_solutions/convert2meters.py
@@ -10,3 +10,15 @@ def convert2meter(s, input_unit="in"):
         return s*0.0283168
     else:
         print("Error: Input unit is unknown.")
+        
+## Apply function
+measurement_unit = {"Girth":"in",
+                    "Height":"ft", 
+                    "Volume":"cft"}
+
+trees = trees_raw.copy()
+for feature in ["Girth", "Height", "Volume"]:
+    trees[feature] = trees_raw[feature].apply(lambda x: convert2meter(s=x,
+                                 input_unit=measurement_unit.get(feature))) 
+                                                 
+                                                                      
